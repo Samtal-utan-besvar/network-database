bb7a67cdda7e0cb8e63a0d4340a6b769188a9219
Folke Johansson <folke.johansson0@gmail.com>
2022-04-05 12:26:03 +0200

    !: refactored database files to be more module based, reading for merge with signal server code.

---
+++ routes/delete.js
@@ -0,0 +1,32 @@
+﻿const authenticateModule = require('../jwt/authenticate');
+const authenticateToken = authenticateModule.authenticateToken;
+const pool = require('../db');
+
+//Delete contact
+function deleteRoutes(app) {
+    app.delete("/delete_contact", authenticateToken, async (req, res) => {
+        try {
+            const { contact_phonenumber } = req.body;
+
+            pool.query(
+                `DELETE FROM CONTACTS 
+                WHERE owner_id = (SELECT user_id FROM USERS WHERE email = $1) 
+                AND contact_user_id = (SELECT user_id FROM USERS WHERE phone_number = $2)`,
+                [req.user.email, contact_phonenumber], (err, result) => {
+                    if (err) {
+                        return console.log('Error executing query during adding of contact' + err.stack);
+                    } else {
+                        res.status(200).send('Deleted Contact!');
+                    }
+                }
+            );
+
+
+        } catch (err) {
+            console.log(err.message);
+            res.status(422).send('Invalid Data ¯\_(ツ)_/¯');
+        }
+    });
+}
+
+module.exports = deleteRoutes;
---
+++ routes/get.js
@@ -0,0 +1,44 @@
+﻿const authenticateModule = require('../jwt/authenticate');
+const authenticateToken = authenticateModule.authenticateToken;
+const generateAccessToken = authenticateModule.generateAccessToken;
+const pool = require('../db');
+
+//Authenticate User
+function getRoutes(app) {
+    app.get("/authenticate", authenticateToken, async (req, res) => {
+        try {
+            const newPayload = {
+                'email': req.user.email
+            }
+
+            res.status(200).send(generateAccessToken(newPayload));
+        } catch (err) {
+            console.log(err.message);
+            res.status(422).send('Invalid Data ¯\_(ツ)_/¯');
+        }
+    });
+
+    //Get Users Contact List
+    app.get("/get_contacts", authenticateToken, async (req, res) => {
+        try {
+            pool.query(
+                `SELECT u.phone_number, u.firstname, u.lastname 
+                FROM CONTACTS c 
+                INNER JOIN USERS u ON c.contact_user_id = u.user_id 
+                WHERE c.owner_id = (SELECT user_id FROM USERS WHERE email = $1)`,
+                [req.user.email], (err, result) => {
+                    if (err) {
+                        return console.log('Error executing query during adding of contact' + err.stack);
+                    } else {
+                        res.status(200).send(result.rows);
+                    }
+                }
+            );
+        } catch (err) {
+            console.log(err.message);
+            res.status(422).send('Invalid Data ¯\_(ツ)_/¯');
+        }
+    });
+}
+
+module.exports = getRoutes;
